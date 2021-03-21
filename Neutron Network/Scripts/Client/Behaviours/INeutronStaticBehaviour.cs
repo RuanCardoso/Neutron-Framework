21b18206e59a206ceddca6cf448dadb100b74896
Ruan Cardoso <61716126+RuanCardoso@users.noreply.github.com>
2021-03-20 23:14:08 -0300

    20/03/21

---
+++ Neutron Network/Scripts/Client/Behaviours/INeutronStaticBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using UnityEngine;
+
+namespace NeutronNetwork
+{
+    public class NeutronStaticBehaviour : MonoBehaviour
+    {
+        public static NeutronStaticBehaviour[] neutronStatics { get; private set; }
+        public MethodInfo[] methods { get; private set; }
+
+        private void OnEnable()
+        {
+            neutronStatics = FindObjectsOfType<NeutronStaticBehaviour>();
+            GetMethods();
+        }
+
+        public void GetMethods()
+        {
+            methods = this.GetType().GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+    }
+}
---
+++ Neutron Network/Scripts/Commons/Attributes/SyncVarAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace NeutronNetwork
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class SyncAttribute : Attribute
+    {
+        public string onChanged;
+        public bool serverOnly;
+        public SendTo sendTo;
+        public Broadcast broadcast;
+        public Protocol protocolType;
+
+        public SyncAttribute(Broadcast broadcast, bool serverOnly = true, SendTo sendTo = SendTo.All, Protocol protocolType = Protocol.Tcp, string onChanged = null)
+        {
+            this.onChanged = onChanged;
+            this.serverOnly = serverOnly;
+            this.broadcast = broadcast;
+            this.protocolType = protocolType;
+            this.sendTo = sendTo;
+        }
+    }
+}
