47325c9e5009c5c206d0a74fc0ae08eb4b353cbc
Ruan Cardoso <cardoso.ruan050322@gmail.com>
2021-10-27 23:20:09 -0300

    added comments

--- Runtime/Base/_SyncVarBehaviour.cs
+++ Runtime/Base/_SyncVarBehaviour.cs
@@ -7,121 +7,126 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-/// <summary>
-///* Criado por: Ruan Cardoso(Brasil)
-///* Os br também são pica.
-///* Email: cardoso.ruan050322@gmail.com
-///* Licença: GNU AFFERO GENERAL PUBLIC LICENSE
-/// </summary>
+//* Created by: Ruan Cardoso(Brasil)
+//* Email: neutron050322@gmail.com
+//* License: MIT
 namespace NeutronNetwork
 {
     /// <summary>
-    ///* Herde para serializar um campo via rede com os attributos: [Sync].<br/>
+    ///* SyncVarBehaviour is a base class for any class that uses SyncVars attributes.
     /// </summary>
     [JsonObject(MemberSerialization.OptOut)]
     public class SyncVarBehaviour : NeutronBehaviour
     {
         #region Fields
-        //*Armazena o json anterior para comparar com o novo.
-        private string _oldJson = "{\"\":\"\"}";
-        //* Armazena os campos e propriedades que serão enviados via rede.
-        private readonly JObject _fieldsAndProperties = new JObject();
-        //* Armazena os campos com o atributo syncvar.
-        private (SyncVarAttribute, FieldInfo)[] _fields;
-        //* Armazena as propriedades com o atributo syncvar.
-        private (SyncVarAttribute, PropertyInfo)[] _properties;
         /// <summary>
-        ///* Configurações de serialização e deserialização.
+        ///* Store the old json value to compare if the value changed.
         /// </summary>
+        /// <value></value>
+        private string _oldJson = "{\"\":\"\"}";
+        /// <summary>
+        ///* Store the fields and properties that will be serialized over the network.
+        /// </summary>
+        /// <returns></returns>
+        private readonly JObject _fieldsAndProperties = new JObject();
+        /// <summary>
+        ///* Store the fields to read are values.
+        /// </summary>
+        private (SyncVarAttribute, FieldInfo)[] _fields;
+        /// <summary>
+        ///* Store the properties to read are values.
+        /// </summary>
+        private (SyncVarAttribute, PropertyInfo)[] _properties;
         protected readonly JsonSerializer JsonSerializer = new JsonSerializer()
         {
-            ObjectCreationHandling = ObjectCreationHandling.Replace,
-            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-            ContractResolver = new SyncVarResolver(),
+            ObjectCreationHandling = ObjectCreationHandling.Replace, //* Replace the object if it already exists.
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore, //* Ignore the reference loop.
+            ContractResolver = new SyncVarContractResolver(), //* Use the SyncVarResolver to serialize the SyncVars.
 
         };
-        /// <summary>
-        ///* Configurações de serialização e deserialização.
-        /// </summary>
         protected readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings()
         {
-            ObjectCreationHandling = ObjectCreationHandling.Replace,
-            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-            ContractResolver = new SyncVarResolver(),
+            ObjectCreationHandling = ObjectCreationHandling.Replace, //* Replace the object if it already exists.
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore, //* Ignore the reference loop.
+            ContractResolver = new SyncVarContractResolver(), //* Use the SyncVarResolver to serialize the SyncVars.
         };
         #endregion
 
         protected virtual void Start()
         {
-            _fields = ReflectionHelper.GetAttributesWithField<SyncVarAttribute>(this);
-            _properties = ReflectionHelper.GetAttributesWithProperty<SyncVarAttribute>(this);
-            SetTokens();
-            _oldJson = _fieldsAndProperties.ToString();
+            _fields = ReflectionHelper.GetAttributesWithField<SyncVarAttribute>(this); //* Get all the fields with SyncVarAttribute.
+            _properties = ReflectionHelper.GetAttributesWithProperty<SyncVarAttribute>(this); //* Get all the properties with SyncVarAttribute.
+            SetTokens(); //* Set the tokens to the fields and properties.
+            _oldJson = _fieldsAndProperties.ToString(); //* Store the old json value.
         }
 
         private void SetTokens()
         {
             foreach (var field in _fields)
-                SetToken(field.Item2.Name, field.Item2.GetValue(this));
+                SetToken(field.Item2.Name, field.Item2.GetValue(this)); //* Set the token to the field.
             foreach (var property in _properties)
-                SetToken(property.Item2.Name, property.Item2.GetValue(this));
+                SetToken(property.Item2.Name, property.Item2.GetValue(this)); //* Set the token to the property.
         }
 
         private void SetToken(string name, object value)
         {
             if (value == null)
                 throw new Exception($"SyncVar: {name} is null!");
-            _fieldsAndProperties[name] = JToken.FromObject(value, JsonSerializer);
+            _fieldsAndProperties[name] = JToken.FromObject(value, JsonSerializer); //* Set the token to the value.
         }
 
+        JObject diffFieldsAndProperties = new JObject(); //* Store the difference between the old and new json.
         public override bool OnAutoSynchronization(NeutronStream stream, bool isMine)
         {
-            var writer = stream.Writer;
-            var reader = stream.Reader;
+            var writer = stream.Writer; //* Get the writer.
+            var reader = stream.Reader; //* Get the reader.
             if (isMine)
             {
-                SetTokens();
-                JObject fieldsAndProperties = new JObject();
-                JToken oldToken = JToken.Parse(_oldJson);
-                JToken currentToken = JToken.Parse(_fieldsAndProperties.ToString());
+                SetTokens(); //* Set the tokens to the fields and properties.
+                diffFieldsAndProperties.RemoveAll(); //* Remove all the fields and properties.
+                JToken oldToken = JToken.Parse(_oldJson); //* Parse the old json value.
+                JToken currentToken = JToken.Parse(_fieldsAndProperties.ToString()); //* Parse the current json value.
                 if (!JToken.DeepEquals(oldToken, currentToken))
                 {
-                    var tokens = currentToken.Except(oldToken, JToken.EqualityComparer).ToArray();
+                    //* If the old and current json values are different, send the current json value.
+                    var tokens = currentToken.Except(oldToken, JToken.EqualityComparer).ToArray(); //* Get the difference between the old and current json values.
                     if (tokens.Length > 0)
                     {
+                        //* If there is a difference, send the difference.
                         foreach (var token in tokens)
-                            fieldsAndProperties.Add(token);
-                        writer.Write(fieldsAndProperties.ToString());
-                        writer.Write();
-                        _oldJson = currentToken.ToString();
+                            diffFieldsAndProperties.Add(token); //* Add the token to the JObject.
+                        writer.Write(diffFieldsAndProperties.ToString()); //* Send the difference.
+                        writer.Write(); //* Send the end of the message.
+                        _oldJson = currentToken.ToString(); //* Store the current json value as the old json value.
                     }
                     else
-                        return false;
+                        return false; //* If there is no difference, return false.
                 }
                 else
-                    return false;
+                    return false; //* If the old and current json values are equal, return false.
             }
             else if (DoNotPerformTheOperationOnTheServer)
-                JsonConvert.PopulateObject(reader.ReadString(), this, JsonSerializerSettings);
-            return OnValidateAutoSynchronization(isMine);
+                JsonConvert.PopulateObject(reader.ReadString(), this, JsonSerializerSettings); //* Read the json value and populate the object.
+            return OnValidateAutoSynchronization(isMine); //* Return the result of OnValidateAutoSynchronization.
         }
 
         protected override bool OnValidateAutoSynchronization(bool isMine) => isMine || OnValidateProperties();
 
         /// <summary>
-        ///* Usado para validar as propriedades ao lado do servidor, disponível somente se a autoridade é do cliente.
+        ///* Used to validate the properties if the client has authority.
         /// </summary>
         /// <returns></returns>
         protected virtual bool OnValidateProperties() => true;
     }
 
-    public class SyncVarResolver : DefaultContractResolver
+    public class SyncVarContractResolver : DefaultContractResolver
     {
         protected override List<MemberInfo> GetSerializableMembers(Type objectType)
         {
+            //* Get all the fields and properties with SyncVarAttribute.
             return base.GetSerializableMembers(objectType)
               .Where(mi => mi.GetCustomAttribute<SyncVarAttribute>() != null)
-              .ToList();
+              .ToList(); //* Return the fields and properties.
         }
     }
 }
