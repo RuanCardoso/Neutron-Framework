57cdeb494697123ac269c0426cf085052e4a5880
Ruan Cardoso <61716126+RuanCardoso@users.noreply.github.com>
2021-11-01 14:16:14 -0300

    Update INeutronConsumer.cs

--- Runtime/Commons/Collections/Serializables/Interfaces/INeutronConsumer.cs
+++ Runtime/Commons/Collections/Serializables/Interfaces/INeutronConsumer.cs
@@ -2,9 +2,9 @@
 {
     public interface INeutronConsumer<T>
     {
-        public void Push(T item);
-        public T Pull();
-        public bool TryPull(out T item);
-        public void Dispose();
+        void Push(T item);
+        T Pull();
+        bool TryPull(out T item);
+        void Dispose();
     }
 }
